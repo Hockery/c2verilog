8e6e46d342358f0f81a5ce9abe11caa63457e49e
Hockery <1291313511@qq.com>
2019-04-26 09:47:36 +0800

    develop: multipy comment

--- c2json.py
+++ c2json.py
@@ -11,11 +11,25 @@ cw_file = open('x265.cpp.test.w', 'w')
 is_sline_comment = False
 is_mline_comment = False
 
+
+def get_comment(line, comment_ms, comment_me):
+
+    if comment_ms < 0 and comment_me < 0:
+        cw_file.write(line)
+    elif comment_ms < 0 and comment_me >= 0:
+        cw_file.write(line[:comment_me+2])
+    elif comment_ms >= 0 and comment_me < 0:
+        cw_file.write(line[comment_ms:])
+    elif comment_ms >= 0 and comment_me >= 0:
+        cw_file.write(line[comment_ms:comment_me])
+    cw_file.write('\n')
+
+
 for line in c_line:
     line = line.strip()
     l_index = 0
-    # is_tm_comment = True
-    while True:
+    is_tm_comment = True
+    while is_tm_comment:
         # 寻找 /* 多行注释开始符号
         comment_ms = line.find('/*', l_index)
         if comment_ms >= 0:
@@ -32,36 +46,33 @@ for line in c_line:
 
         if comment_ms >= 0:
             is_mline_comment = True
-        
+
+        print(comment_me, comment_ms, is_mline_comment, end=' ')
+        print(l_index, len(line))
 
         # 如果查阅长度大于了字符长度，退出
         if l_index >= len(line):
-            break
-        # 如果是多行注释，
-        if not is_mline_comment:
-            continue
+            is_tm_comment = False
 
-        if  comment_ms < 0 and comment_me < 0:
+        # 如果该行没有多行注释开始符号并且多行注释也没有没有在本行，就退出
+        if comment_ms < 0 and not is_mline_comment:
+            break
+
+        if comment_ms < 0 and comment_me < 0:
             cw_file.write(line)
         elif comment_ms < 0 and comment_me >= 0:
-            cw_file.write(line[:comment_me+1])
+            cw_file.write(line[:comment_me+2])
         elif comment_ms >= 0 and comment_me < 0:
             cw_file.write(line[comment_ms:])
-        elif comment_ms >=0 and comment_me >= 0:
-            cw_file.write(line[comment_ms:comment_me+1])
+        elif comment_ms >= 0 and comment_me >= 0:
+            cw_file.write(line[comment_ms:comment_me])
         cw_file.write('\n')
 
-        
-        print(l_index, len(line))
-
-        print(comment_me, comment_ms, is_mline_comment, end=' ')
         if comment_me >= 0 and is_mline_comment:
             is_mline_comment = False
-
-        # 如果改行没有多行注释命令了，就退出
+        # 如果改行没有多行注释符号了，说明该行已经分析完成， 就退出
         if comment_ms < 0 and comment_me < 0:
-            break 
-
+            break
 
     # comment_s = line.find('//', l_index)
     # if comment_s > 0:
